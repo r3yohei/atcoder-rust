4ee377e30f5d1b17cb7b7082d55b767775df2f60
r3yohei <r3yohei@gmail.com>
2022-07-12 14:02:18 +0900

    dfsをわかりやすくする

--- graph/dfs.rs
+++ graph/dfs.rs
@@ -1,15 +1,14 @@
 // DFS
-// main関数内で使用するときはよく，dfs(start, -1, グラフの隣接リスト表現, 訪問フラグ)
-// のようにする
-fn dfs(current: usize, prev: usize, edges: &Vec<Vec<usize>>, mut visited: &mut Vec<bool>) {
-    visited[current] = true;
+// dfs(0, std::usize::MAX, &edges, &mut visited);
+fn dfs(cur: usize, pre: usize, edges: &Vec<Vec<usize>>, mut visited: &mut Vec<bool>) {
+    visited[cur] = true;
     // 行きがけにやりたいことをここに書く
-    for &to in &edges[current] {
+    for &to in &edges[cur] {
         // すでに見た頂点を行き先に設定しない
         if visited[to] {
             continue;
         }
-        dfs(to, current, edges, visited);
+        dfs(to, cur, edges, visited);
     }
     // 帰りがけにやりたいことをここに書く
 }
